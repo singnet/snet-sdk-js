5304f6a471dcb271f9b0b8e72522996e88830ef5
chetankothari <chetankothari@users.noreply.github.com>
2019-04-10 17:28:58 +0530

    Add mpe open channel

---
+++ src/MPEContract.js
@@ -0,0 +1,24 @@
+import MPEAbi from 'singularitynet-platform-contracts/abi/MultiPartyEscrow';
+import MPENetworks from 'singularitynet-platform-contracts/networks/MultiPartyEscrow';
+
+export default class MPEContract {
+  constructor(web3, account, config) {
+    this._web3 = web3;
+    this._account = account;
+    this._config = config;
+    this._contract = this._web3.eth.Contract(MPEAbi, MPENetworks[this._config.networkId].address);
+  }
+
+  get contract() {
+    return this._contract;
+  }
+
+  get address() {
+    return this.contract.address;
+  }
+
+  async openChannel(signerAddress, recipientAddress, groupId, amount, expiration) {
+    const openChannelOperation = this.contract.methods.openChannel(signerAddress, recipientAddress, groupId, amount, expiration);
+    return this._account.sendSignedTransaction(openChannelOperation, this.address);
+  }
+}
