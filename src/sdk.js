d9756ce6575a100d368df20be0f735ffc503291a
chetankothari <chetankothari@users.noreply.github.com>
2019-04-10 16:52:06 +0530

    Fix channel management strategy creation

--- src/sdk.js
+++ src/sdk.js
@@ -40,7 +40,7 @@ export default class SnetSDK {
   async createServiceClient(orgId, serviceId, ServiceStub) {
     const serviceMetadata = await this._getServiceMetadata(orgId, serviceId);
     this._serviceClient.metadata = serviceMetadata;
-    const channelManagementStrategy = new ChannelManagementStrategy(this._web3, this._config, serviceMetadata);
+    const channelManagementStrategy = new ChannelManagementStrategy(this._web3, this._account, this._config, serviceMetadata);
     await channelManagementStrategy.setup();
 
     const grpcOptions = {
