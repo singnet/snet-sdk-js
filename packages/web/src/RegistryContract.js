6307831224bf47e7f64bd5cf37d8f8756fc72b30
Vivek205 <n.vin95@gmail.com>
2020-02-27 12:31:56 +0530

    included getServiceRegistrationById in the registry contract

--- packages/web/src/RegistryContract.js
+++ packages/web/src/RegistryContract.js
@@ -127,6 +127,23 @@ class RegistryContract {
     const enhancedOrgId = this._web3.utils.fromAscii(orgId);
     return this._contract.methods.getOrganizationById(enhancedOrgId);
   }
+
+  /**
+   *
+   * @param {string} orgId  Id of the organization that owns the service to look up.
+   * @param {string} serviceId Id of the service to look up.
+   *
+   * @returns {boolean} true if an organization and service with these ids exists, false otherwise. If false, all other
+   *                      returned fields should be ignored.
+   * @returns {string}  Id of the service, should be the same as the serviceId parameter.
+   * @returns {string} metadataURI  Service metadata URI
+   * @returns {string[]} serviceTags  Optional array of tags for discoverability.
+   */
+  getServiceRegistrationById(orgId, serviceId) {
+    const enhancedOrgId = this._web3.utils.fromAscii(orgId);
+    const enhancedServiceId = this._web3.utils.fromAscii(serviceId);
+    return this._contract.methods.getServiceRegistrationById(enhancedOrgId, enhancedServiceId);
+  }
 }
 
 export default RegistryContract;
